a15474e0c0e7110ede0de01d7cd98dabab3ed6c7
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-11-14 15:38:16 -0300

    add associations to the models - add new food form

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,5 @@
 class Food < ApplicationRecord
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :food_recipes
+  has_many :recipes, through: :food_recipes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
 
+  has_many :foods
+  has_many :recipes
+
   validates :name, presence: true
 end
