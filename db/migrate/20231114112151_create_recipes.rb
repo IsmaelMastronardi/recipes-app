f56fe8a4c6e8ad7a27e62a6b27c8d73dc3d648fc
devalibello <alibelloimoukhuede@gmail.com>
2023-11-14 15:15:57 +0300

    Add MVC for users and recipes

--- db/migrate/20231114112151_create_recipes.rb
+++ db/migrate/20231114112151_create_recipes.rb
@@ -4,8 +4,8 @@ class CreateRecipes < ActiveRecord::Migration[7.1]
       t.string :name
       t.string :preparation_time
       t.string :cooking_time
-      t.string :description
-      t.string :public
+      t.text :description
+      t.boolean :public
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+# 3.times do |i|
+#     User.create(
+#         name: "User#{i}"
+#     )
+# end
+user = User.first
+Recipe.create(
+    name: "burger",
+    preparation_time: "20 minutes",
+    cooking_time: "2 minutes",
+    description: "The best meal ever!",
+    public: true,
+    user: user
+)
