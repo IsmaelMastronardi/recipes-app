8f42e5256bc4f277ed360db414ad65874b7eb190
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-11-16 21:13:09 -0300

    add test for remove ingredient in recipe details

--- spec/system/food_recipe_destroy_spec.rb
+++ spec/system/food_recipe_destroy_spec.rb
@@ -1,18 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe 'Recipe show', type: :system do
+RSpec.describe 'Food creation', type: :system do
   let(:user) do
     User.create(name: 'example_user', email: 'user@example.com', password: 'password', id: 1)
   end
+
   let(:food) do
-    Food.create(
-      name: 'apple',
-      measurement_unit: 'grams',
-      price: '10',
-      quantity: '2',
-      user_id: user.id
-    )
+    Food.create(name: 'apple', measurement_unit: 'grams', price: 5, quantity: 10, user_id: user.id)
   end
+
   let(:recipe) do
     Recipe.create(
       name: 'test recipe',
@@ -32,18 +28,15 @@ RSpec.describe 'Recipe show', type: :system do
     user.confirm
     sign_in user
   end
-  it 'displays the details of a recipe' do
+
+  it 'allows a user to update a food' do
     visit recipe_path(recipe)
-    expect(page).to have_content('test recipe')
-    expect(page).to have_content('2 hours')
-    expect(page).to have_content('1 hour')
-    expect(page).to have_content('this is a test description')
-  end
-  it 'displays the details of a recipes food' do
-    visit recipe_path(recipe)
-    
+    # Before delete
     expect(page).to have_content('apple')
-    expect(page).to have_content('10')
-    expect(page).to have_content('2')
+    
+    click_button 'Remove'
+
+    # After delete
+    expect(page).to_not have_content('apple')
   end
 end
