b986648e0028bce938e8c656f2737a7bc3f3ce29
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-11-18 10:56:48 -0300

    fix linters issues

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,7 +11,7 @@ class RecipesController < ApplicationController
   # GET /recipes/1 or /recipes/1.json
   def show
     nil unless user_signed_in?
-    
+
     @recipe = Recipe.includes(:foods, :food_recipes).find(params[:id])
   end
 
